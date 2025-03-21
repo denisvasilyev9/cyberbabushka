fc1033cd368a341b06b7a1dc784cb0e09e9f9021
denisvasilyev9 <denisvasilyev9@gmail.com>
2025-03-21 03:06:18 +0300

    Create app.py

---
+++ templates/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+responses = {
+    "привет": "Здорово, внучек!",
+    "как дела": "Да вот, железяку новую починила!",
+    "где купить хлеб": "Да в Яндекс Лавке закажи, чо бегать-то!"
+}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get_response")
+def get_response():
+    msg = request.args.get("msg", "").lower()
+    response = responses.get(msg, "Чё ты там буровишь, внучек?")
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
